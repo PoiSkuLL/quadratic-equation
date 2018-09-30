5cf2b68a3aa5f96d64969f317d664594856b2417
PoiSkuLL <poiskull@gmail.com>
2018-09-30 22:38:57 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    const expression = equation.split(' ');
+    let result = [];
+    const a = parseInt(expression[0]);
+    const b = parseInt(expression[3] + expression[4]);
+    const c = parseInt(expression[7] + expression[8]);
+    const D = Math.sqrt((b * b) - (4 * a * c));
+    result.push(Math.round((-b - D) / (2 * a)));
+    result.push(Math.round((-b + D) / (2 * a)));
+    return (result[0] > result[1]) ? result.reverse() : result;
 }
